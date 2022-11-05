5f85bad3a364468ec845ce5e4405af8f6ea2acb0
Adil <ajkkalappatt@gmail.com>
2022-11-05 17:07:41 +0530

    added linked list insertation and traverse

---
+++ Queue/Linkedlist/linkedListOwnMy.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void main(){
+    struct node{
+        int data;
+        struct node *next;
+    };
+    struct node *head,*newNode,*temp;
+    int choice,count=0;
+    head = 0;
+    while(choice == 0){
+        newNode =(struct node*)malloc(sizeof(struct node));
+        printf("enter data");
+        scanf("%d",&newNode -> data);
+        newNode -> next =0;
+        if(head ==0){
+            head = temp = newNode;
+        }else{
+            temp -> next = newNode;
+            temp = newNode;
+        }
+        printf("do you want to continue (0,1)");
+        scanf("%d",&choice);
+    }
+    temp = head;
+    while (temp !=0)
+    {
+        printf("%d\n", temp -> data);
+        temp = temp -> next;
+        count++;
+    }
+        //display the list
+        
+        
+}
